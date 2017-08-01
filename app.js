a36f7467340e489fd4f523ed36359d9197741210
Soo Young Kim <syk@Soos-MacBook-Air.local>
2017-08-01 15:46:49 -0400

    end of day

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const nunjucks = require( 'nunjucks' );
 const _ = require('lodash');
 const app = express(); 
 const routes = require('./routes');
+var bodyParser = require('body-parser')
 
 app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
 nunjucks.configure('views', { noCache: true }); // point nunjucks to the proper directory for templates
+
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false }))
 
 
 
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,21 @@ const tweetBank = require('../tweetBank');
 
 router.get('/', function (req, res) {
   let tweets = tweetBank.list();
-  res.render( 'index', { tweets: tweets } );
+  res.render( 'index', { tweets: tweets, showForm: true } );
+});
+
+
+router.get('/users/:name', function(req, res) {
+  var name = req.params.name;
+  var list = tweetBank.find( {name: name} );
+  res.render( 'index', { tweets: list } );
+});
+
+router.post('/tweets', function(req, res) {
+  var name = req.body.name;
+  var text = req.body.text;
+  tweetBank.add(name, text);
+  res.redirect('/');
 });
 
 module.exports = router;
